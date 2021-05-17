e7a8d909e6da877f9db421660f8608f47518699a
valentynkuts <valik.kuts@gmail.com>
2021-05-17 08:26:35 +0200

    draw point which describe sin

--- 01_hello_sdl/src/main.cpp
+++ 01_hello_sdl/src/main.cpp
@@ -33,7 +33,7 @@ int main(int, char **)
   using namespace std;
   using namespace std::chrono;
   const int width = 808;  //640;
-  const int height = 808; //8480;
+  const int height = 808; //480;
 
   errcheck(SDL_Init(SDL_INIT_VIDEO) != 0);
 
@@ -50,15 +50,6 @@ int main(int, char **)
                                       SDL_DestroyRenderer(r);
                                     });
 
-  // SDL_Window *window = SDL_CreateWindow(
-  //     "Hello my first Game!!!!", SDL_WINDOWPOS_UNDEFINED,
-  //     SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-  // errcheck(window == nullptr);
-
-  // SDL_Renderer *renderer = SDL_CreateRenderer(
-  //     window, -1, SDL_RENDERER_ACCELERATED); // SDL_RENDERER_PRESENTVSYNC
-  // errcheck(renderer == nullptr);
-
   //auto dt = 15ms;
   milliseconds dt(15);
 
@@ -68,10 +59,7 @@ int main(int, char **)
   {
     SDL_Event event;
     while (SDL_PollEvent(&event))
-    { // check if there are some events
-      //if (event.type == SDL_QUIT)
-      //  game_active = false;
-
+    { 
       switch (event.type)
       {
         //close window by press X (right top corner)
@@ -93,21 +81,19 @@ int main(int, char **)
 
     x += 0.6;
     y = sin(0.1 * x / M_PI) * 200 + 240;
-    
-    //SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
-    //SDL_RenderClear(renderer.get());
-    
+
+    SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
+    SDL_RenderClear(renderer.get());
+
     SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255);
     //SDL_RenderDrawPoint(renderer.get(), x, y);
     SDL_RenderDrawLineF(renderer.get(), x, y, px, py);
 
     SDL_RenderPresent(renderer.get()); // draw frame to screen
-    //SDL_RenderPresent(renderer); // draw frame to screen
 
     this_thread::sleep_until(current_time = current_time + dt);
   }
-  //SDL_DestroyRenderer(renderer);  // it is for SDL_Renderer *renderer
-  //SDL_DestroyWindow(window); //  for SDL_Window *window
+
   SDL_Quit();
   return 0;
 }
